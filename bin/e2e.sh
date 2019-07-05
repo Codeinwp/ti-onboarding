675e2808163186a5e46854d2ee7360450eb6a809
Andrei Baicus <baicusandrei@me.com>
2019-07-05 19:36:35 +0300

    chore: cypress integration [wip]

---
+++ bin/e2e.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+export DOCKER_FILE=cypress/docker-compose.yml
+
+# Bring up the site container
+docker-compose -f $DOCKER_FILE up -d
+sleep 10
+docker-compose -f $DOCKER_FILE run --rm cli wp core install --url=http://localhost:8056 --title=SandboxSite --admin_user=admin --admin_password=admin --admin_email=admin@admin.com
+docker-compose -f $DOCKER_FILE run --rm -u root cli chmod 0777 -R /var/www/html
+docker-compose -f $DOCKER_FILE run --rm cli wp theme install neve
+docker-compose -f $DOCKER_FILE run --rm cli wp theme activate neve
+
+npm run cypress:run || export $BUILD_EXIT
+
+## Bring down the site container.
+docker-compose -f $DOCKER_FILE down
+## Bring down the backstop container.
+exit $BUILD_EXIT
+
