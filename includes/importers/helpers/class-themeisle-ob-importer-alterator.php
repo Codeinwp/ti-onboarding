d6bdc36c696471ded63defcd14c22626e2c6cab2
abaicus <baicusandrei@me.com>
2020-01-29 14:05:43 +0200

    chore: fix unavailable array index

--- includes/importers/helpers/class-themeisle-ob-importer-alterator.php
+++ includes/importers/helpers/class-themeisle-ob-importer-alterator.php
@@ -99,7 +99,7 @@ class Themeisle_OB_Importer_Alterator {
 	 * @return array
 	 */
 	public function encode_post_content( $data, $postarr ) {
-		if ( $this->site_json_data['editor'] === 'gutenberg' ) {
+		if ( isset( $this->site_json_data['editor'] ) && $this->site_json_data['editor'] === 'gutenberg' ) {
 			return $data;
 		}
 		$data['post_content'] = utf8_encode( $data['post_content'] );
